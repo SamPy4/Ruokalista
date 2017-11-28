bd399bc51191f827c9431bb9dd31936f83e69280
SamPy4 <sami.porio@gmail.com>
2017-11-28 13:59:33 +0200

    10.2.2

--- main_ruokalista.py
+++ main_ruokalista.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 # TODO: Ikkuna levittää itsensä niin että teksti pomppaa pois näkyvistä
 # TODO: Sisältää tyhmän "maanantai" = 0 = maanantai käännöksen
-# TODO: Päivät joiden mukaan viikot on märätty ovat kovakoodattu, voisi tehdä sen pdf:n käsittelyssä
 
 class main():
     def __init__(self):
